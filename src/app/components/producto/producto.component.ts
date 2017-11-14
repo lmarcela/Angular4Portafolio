f06aef8fca9f15d56ff93a565dfe3785bd75e80f
Marcela Malaver <marcela9409@gmail.com>
2017-11-14 12:45:04 -0500

    Product component created

---
+++ src/app/components/producto/producto.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html',
+  styles: [],
+})
+export class ProductoComponent{
+}
