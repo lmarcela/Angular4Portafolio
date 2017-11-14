96ba4244c30fa433b800b115c117608169232cba
Marcela Malaver <marcela9409@gmail.com>
2017-11-14 12:33:40 -0500

    About created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { PortafolioComponent } from './components/portafolio/portafolio.componen
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styles: [],
+})
+export class AboutComponent{
+}
