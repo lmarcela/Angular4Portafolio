0f6186b39a0d493d0f568ab4141d24addfdb89ba
Marcela Malaver <marcela9409@gmail.com>
2017-11-14 13:34:01 -0500

    Routes created

---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,3 @@
+export {AboutComponent} from "./about/about.component";
+export {PortafolioComponent} from "./portafolio/portafolio.component";
+export {ProductoComponent} from "./producto/producto.component";
