b8cb33ccdde56e5ddf09b85ed22ac52ddbea545d
Marcela Malaver <marcela9409@gmail.com>
2017-11-14 12:05:05 -0500

    Header, Body, Footer Ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/portafolio/portafolio.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio',
+  templateUrl: './portafolio.component.html',
+  styles: [],
+})
+export class PortafolioComponent{
+}
