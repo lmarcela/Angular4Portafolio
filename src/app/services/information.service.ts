35c73953f49aaf63a8519fa49571914f0baa5268
Marcela Malaver <marcela9409@gmail.com>
2017-11-14 20:38:16 -0500

    Firebase service ready

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -6,12 +6,26 @@ export class InformationService {
 
   info: any = {};
   cargada : boolean = false;
+  cargadaSobreNosotros : boolean = false;
+  equipo: any[]=[];
 
   constructor( public http:Http ) { 
+    this.cargaInfo();
+    this.cargaSobreNosotros();
+  }
+
+  public cargaInfo(){
     this.http.get("assets/data/info.pagina.json").subscribe( data =>{
-      console.log(data.json());
+      //console.log(data.json());
       this.cargada = true;
       this.info = data.json();
+    })
+  }
+  public cargaSobreNosotros(){
+    this.http.get("https://paginaweb-549ff.firebaseio.com/equipo.json").subscribe( data =>{
+      console.log(data.json());
+      this.cargadaSobreNosotros = true;
+      this.equipo = data.json();
     })
   }
 
